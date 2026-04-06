ffd1e6340232544cbe084c885dbd26f5d611789c
Tên của bạn <email@gmail.com>
2026-04-06 14:02:03 +0700

    bug: break add function

--- src/calculator.js
+++ src/calculator.js
@@ -10,10 +10,11 @@ function multiply(a, b) {
   return a * b;
 }
 
-function divide(a, b) {
-  if (b === 0) throw new Error("Cannot divide by zero");
-  return a / b;
+function add(a, b) {
+  console.log("Adding:", a, b);
+  return a + b + 10;
 }
+
 function add(a, b) {
   console.log("Adding:", a, b);
   return a + b;
